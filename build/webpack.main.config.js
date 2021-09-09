d381a481faab86828479b4cf8c6f4176e0036a77
williamnie <yebeixiaohai@gmail.com>
2021-09-09 11:48:49 +0800

    fix merge bug

--- build/webpack.main.config.js
+++ build/webpack.main.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const { merge } = require('webpack-merge');
+const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.config');
 
 module.exports = merge(baseConfig, {
