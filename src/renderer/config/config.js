6edb56acfe36c4fad6cd8ab336f0ef275478b3fa
williamnie <yebeixiaohai@gmail.com>
2022-01-29 22:02:58 +0800

    升级umi4.0

--- src/renderer/config/config.js
+++ src/renderer/config/config.js
@@ -4,17 +4,20 @@ export default {
   history: { type: 'hash' },
   outputPath: `../../dist/renderer`,
   publicPath: './',
-  title: 'Electron',
   nodeModulesTransform: { type: 'none' },
-  webpack5: {},
-  mfsu: {},
 
   alias: buildrc.webpack.alias,
   ignoreMomentLocale: true,
-  routes: [
-    {
-      path: '/',
-      component: './index',
-    },
-  ],
+
+
+  model: {},
+  antd: {},
+  request: {},
+  initialState: {},
+  locale: {
+    default: 'zh-CN',
+    antd: true,
+    baseNavigator: true,
+  },
+
 };
---
+++ src/renderer/pages/index.tsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+
+const Index = (props) => {
+  return <div>Index Page</div>;
+};
+export default Index;
